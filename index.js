db4a97ee61ab5ab011b8cf12c36b30d6a582d5da
mubashirbappi <mubashirbappi@gmail.com>
2024-12-23 01:32:12 +0600

    first get req for testing purpose

--- index.js
+++ index.js
@@ -21,14 +21,25 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+
+    const volunteerdatabase = client.db("volunteerdb").collection("volunteerneedposts");
+     
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
+
+
+    app.get('/posts',async(req,res)=>{
+        const cursor = volunteerdatabase.find()
+      const result = await cursor.toArray();
+      res.send(result)
+    })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
