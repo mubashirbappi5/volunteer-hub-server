9adf4222b96692d4120ba325e1b243db9434bda8
mubashirbappi <mubashirbappi@gmail.com>
2024-12-25 03:01:24 +0600

    added update put request

--- index.js
+++ index.js
@@ -70,6 +70,29 @@ async function run() {
       const result = await volunteerdatabase.deleteOne(quary)
       res.send(result)
     })
+    app.put('/posts/:id',async(req,res)=>{
+      const id = req.params.id
+      const filter = {_id:new ObjectId(id)}
+      const options = { upsert: true };
+      const updatepost = req.body
+      const updateposts ={
+        $set:{
+          title:updatepost.title,
+          volunteers_needed:updatepost.volunteers_needed,
+          deadline:updatepost.deadline,
+          description:updatepost.deadline,
+          category:updatepost.category,
+          organizer_email:updatepost.organizer_email,
+          organizer_name:updatepost.organizer_name,
+          location:updatepost.location,
+          thumbnail:updatepost.thumbnail,
+          
+
+        }
+      }
+      const result = await volunteerdatabase.updateOne(filter, updateposts, options)
+      res.send(result)
+    })
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
